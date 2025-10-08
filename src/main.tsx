df0709300e0f1eb69e603bee6a6a507afc05b3c2
chenxing-dev <dev.chenxing@gmail.com>
2025-10-08 12:41:39 +0800

    Refactor styles: move index.css to styles directory and add animations.css for visual effects

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/index.css'
 import App from './App.tsx'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
